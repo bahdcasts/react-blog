a44d4b3dcaf9eb259a875d0be131727c5d1d6afd
Kati Frantz <frantzenterprises777@gmail.com>
2018-03-18 06:41:14 +0100

    add browser router and three pages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Home = () => {
+  return <h1>THIS IS THE HOME PAGE</h1>
+};
+
+const About = () => {
+  return <h1>THIS IS THE ABOUT PAGE</h1>
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/home" component={Home} />
+    </div>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
