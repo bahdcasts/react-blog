d02e390f503c00340b4e46948408b21df5b06ba5
Kati Frantz <frantzenterprises777@gmail.com>
2018-03-18 06:50:30 +0100

    add Link components to navigate different pages in react apps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -16,6 +16,17 @@ const About = () => {
 ReactDOM.render(
   <BrowserRouter>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Root page</Link>
+        </li>
+        <li>
+          <Link to="/about">About page</Link>
+        </li>
+        <li>
+          <Link to="/home">Home page</Link>
+        </li>
+      </ul>
       <Route exact path="/" component={App} />
       <Route path="/about" component={About} />
       <Route path="/home" component={Home} />
