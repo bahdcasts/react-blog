d9e5208d0a4e9a73818e19765a0e7c45314a72f5
Kati Frantz <frantzenterprises777@gmail.com>
2018-03-18 08:03:50 +0100

    add article component render multiple article components to home screen

---
+++ src/components/Article/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <article className="mt-90">
+      <header className="text-center mb-40">
+        <h3>
+          <a href="blog-single.html">New features will add to dashboard soon</a>
+        </h3>
+        <div className="link-color-default fs-12">
+          <a href="#">News</a>,
+      <time>May 13, 2017</time>
+        </div>
+      </header>
+      <a href="blog-single.html">
+        <img className="rounded" src="assets/img/blog-1.jpg" alt="..." />
+      </a>
+      <div className="card-block">
+        <p className="text-justify">Together. Great. So good was saying, that can't first let called air divide stars male isn't i. Herb third let
+          may fourth divide. Greater gathering land you'll i their beast have. She'd form sea it wherein fowl, spirit
+          creeping living. Likeness creepeth you hath heaven. Likeness, moveth fruitful behold. Open evening a air us
+      behold. Saying above moving second a subdue likeness after also second.</p>
+        <p className="text-center mt-40">
+          <a className="btn btn-primary btn-round" href="blog-single.html">Read more</a>
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default Article;
--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
 
+import Article from './../Article';
+
 const Welcome = () => {
   return (
-    <header className="header header-inverse" style={{ backgroundImage: 'url(assets/img/bg-gift.jpg)' }} data-overlay={8}>
-      <div className="container text-center">
-        <div className="row">
-          <div className="col-12 col-lg-8 offset-lg-2">
-            <h1>Latest Blog Posts</h1>
-            <p className="fs-20 opacity-70">Read and get updated on how we progress.</p>
+    <div>
+      <header className="header header-inverse" style={{ backgroundImage: 'url(assets/img/bg-gift.jpg)' }} data-overlay={8}>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-12 col-lg-8 offset-lg-2">
+              <h1>Latest Blog Posts</h1>
+              <p className="fs-20 opacity-70">Read and get updated on how we progress.</p>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <main className="main-content bg-gray">
+        <div className="row">
+          <div className="col-12 col-lg-6 offset-lg-3">
+            <Article />
+            <hr/>
+            <Article />
+            <hr/>
+            <Article />
+            <hr/>
+            <Article />    
+            <hr/>        
+          </div>
+        </div>
+      </main>
+    </div>
   );
 };
 
