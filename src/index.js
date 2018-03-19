bab003b5eb9b385e0bbb590f700f806b3c4e4676
Kati Frantz <frantzenterprises777@gmail.com>
2018-03-19 05:16:43 +0100

    add login component add login route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import Login from './components/Login'
 import Footer from './components/Footer';
 import Welcome from './components/Welcome';
 import CreateArticle from './components/CreateArticle';
@@ -24,6 +25,7 @@ ReactDOM.render(
       <Route exact path="/" component={Welcome} />
       <Route path="/about" component={About} />
       <Route path="/home" component={Home} />
+      <Route path="/login" component={Login} />      
       <Route path="/articles/create" component={CreateArticle} />
       <Footer />
     </div>
